97d9e12740fe2b4f16adf77921f8f6beeba9b7d4
Gi0vanniM <g.l.mierop@gmail.com>
2021-04-25 15:40:30 +0200

    added song source to migration

--- database/migrations/2021_04_25_102747_create_songs_table.php
+++ database/migrations/2021_04_25_102747_create_songs_table.php
@@ -16,6 +16,7 @@ class CreateSongsTable extends Migration
         Schema::create('songs', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('song_src');
             $table->integer('duration');
             $table->foreignId('genre_id')->nullable()->constrained(); // will link to the genres id column
             $table->timestamps();
