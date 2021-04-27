578f69662a0aee41514c3cb666f54a47b6beef0a
Gi0vanniM <g.l.mierop@gmail.com>
2021-04-27 15:00:23 +0200

    PlaylistSong Controller, Model and Factory (migration already exists)

---
+++ database/factories/PlaylistSongFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\PlaylistSong;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PlaylistSongFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = PlaylistSong::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
