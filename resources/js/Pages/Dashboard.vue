420a9c6c973539e583b4cdd2064bac3f8ce4ef8d
Gi0vanniM <g.l.mierop@gmail.com>
2021-04-28 17:57:04 +0200

    emptied dashboard view

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -6,18 +6,15 @@
             </h2>
         </template>
 
-        <welcome />
     </app-layout>
 </template>
 
 <script>
     import AppLayout from '@/Layouts/AppLayout'
-    import Welcome from '@/Jetstream/Welcome'
 
     export default {
         components: {
             AppLayout,
-            Welcome,
         },
     }
 </script>
