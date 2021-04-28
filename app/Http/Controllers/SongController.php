4b33065a1feb7e6897856e03e9a2666626b09630
Gi0vanniM <g.l.mierop@gmail.com>
2021-04-28 17:56:23 +0200

    Songs index

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Song;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class SongController extends Controller
 {
@@ -14,7 +15,9 @@ class SongController extends Controller
      */
     public function index()
     {
-        //
+        return Inertia::render('Song/Index', [
+            'songs' => Song::all(),
+        ]);
     }
 
     /**
---
+++ resources/js/Pages/Song/Index.vue
@@ -0,0 +1,27 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h1 class="font-weight-bold">Songs</h1>
+        </template>
+        <ul v-for="song in songs" :key="song" class="list-group">
+            <a href="#" class="list-group-item list-group-item-action">
+                {{ song.name }}
+            </a>
+        </ul>
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from "@/Layouts/AppLayout";
+export default {
+    components: {
+        AppLayout,
+    },
+    props: {
+        songs: Array,
+    },
+};
+</script>
+
+<style>
+</style>
