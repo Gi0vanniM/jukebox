ded86f946331ebe43c395f2c0c2ab13186bf1c54
Gi0vanniM <g.l.mierop@gmail.com>
2021-04-28 17:56:03 +0200

    updated genre index view

--- resources/js/Pages/Genre/Index.vue
+++ resources/js/Pages/Genre/Index.vue
@@ -1,17 +1,27 @@
 <template>
-    <div v-for="genre in genres" :key="genre">
-        {{ genre.name }}
-    </div>
+    <app-layout>
+        <template #header>
+            <h1 class="font-weight-bold">Genres</h1>
+        </template>
+        <ul v-for="genre in genres" :key="genre" class="list-group">
+            <a href="#" class="list-group-item list-group-item-action">
+                {{ genre.name }}
+            </a>
+        </ul>
+    </app-layout>
 </template>
 
 <script>
+import AppLayout from "@/Layouts/AppLayout";
 export default {
+    components: {
+        AppLayout,
+    },
     props: {
         genres: Array,
-    }
-}
+    },
+};
 </script>
 
 <style>
-
 </style>
