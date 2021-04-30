fc4e46e611134a92812226795607433b81037d8f
Gi0vanniM <g.l.mierop@gmail.com>
2021-04-30 18:04:45 +0200

    using pagination component on songs index

--- resources/js/Pages/Song/Index.vue
+++ resources/js/Pages/Song/Index.vue
@@ -3,19 +3,29 @@
         <template #header>
             <h1 class="font-weight-bold">Songs</h1>
         </template>
-        <ul v-for="song in songs" :key="song" class="list-group">
-            <a href="#" class="list-group-item list-group-item-action">
-                {{ song.name }}
+        <ul class="list-group">
+            <a
+                v-for="song in songs.data"
+                :key="song.id"
+                href="#"
+                class="list-group-item list-group-item-action"
+            >
+                {{ song.name + " - " + song.artist.name }}
             </a>
         </ul>
+
+    <pagination :links="songs.links"/>
+
     </app-layout>
 </template>
 
 <script>
 import AppLayout from "@/Layouts/AppLayout";
+import Pagination from "@/Components/Pagination";
 export default {
     components: {
         AppLayout,
+        Pagination,
     },
     props: {
         songs: Array,
