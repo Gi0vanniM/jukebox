7c396b19ecd8c49b946874ddd8c10ea7c330ff67
Gi0vanniM <g.l.mierop@gmail.com>
2021-05-04 14:38:32 +0200

    using a table instead to display songs

--- resources/js/Pages/Song/Index.vue
+++ resources/js/Pages/Song/Index.vue
@@ -3,16 +3,23 @@
         <template #header>
             <h1 class="font-weight-bold">Songs</h1>
         </template>
-        <ul class="list-group">
-            <a
-                v-for="song in songs.data"
-                :key="song.id"
-                href="#"
-                class="list-group-item list-group-item-action"
-            >
-                {{ song.name + " - " + song.artist.name }}
-            </a>
-        </ul>
+
+        <table class="table table-hover border">
+            <thead>
+                <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Artist</th>
+                    <th scope="col"><i class="far fa-clock"></i></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="song in songs.data" :key="song.id">
+                    <td>{{ song.name }}</td>
+                    <td>{{ song.artist.name }}</td>
+                    <td>{{ secondsToTime(song.duration) }}</td>
+                </tr>
+            </tbody>
+        </table>
 
     <pagination :links="songs.links"/>
 
@@ -30,6 +37,11 @@ export default {
     props: {
         songs: Array,
     },
+    methods: {
+        secondsToTime(seconds) {
+            return new Date(seconds * 1000).toISOString().substr(11, 8).replace(/^0(?:0:0?)?/, '');
+        }
+    }
 };
 </script>
 
