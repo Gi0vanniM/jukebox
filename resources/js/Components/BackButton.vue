cf6a653432f544ca17ac1a90814bdc770652202b
Gi0vanniM <g.l.mierop@gmail.com>
2021-05-28 11:54:35 +0200

    Back button component

---
+++ resources/js/Components/BackButton.vue
@@ -0,0 +1,28 @@
+<template>
+    <button v-on:click="goBack()" class="btn back-button">
+        <i class="fas fa-arrow-left"></i>
+    </button>
+</template>
+
+<script>
+export default {
+    methods: {
+        goBack() {
+            window.history.back();
+        }
+    }
+};
+</script>
+
+<style>
+    .back-button {
+        color: black;
+        border-radius: 50%;
+        border-width: 1px;
+        border-style: solid;
+        border-color: black;
+    }
+    .back-button:hover {
+        background-color: rgb(50, 159, 255);
+    }
+</style>
