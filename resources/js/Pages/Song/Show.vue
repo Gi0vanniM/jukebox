ba1e53a4d9597b214f7dd3a49893ea03a00ff354
Gi0vanniM <g.l.mierop@gmail.com>
2021-06-06 14:06:23 +0200

    added more details to song-detail page also in a nicer style

--- resources/js/Pages/Song/Show.vue
+++ resources/js/Pages/Song/Show.vue
@@ -1,20 +1,48 @@
 <template>
     <app-layout v-bind:title="song.name">
-
         <template #header>
-            <back-button/>
-            <h1 class="font-weight-bold">
-                {{ song.name }}
-            </h1>
+            <back-button />
         </template>
+
+        <div class="card border">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <h1 class="font-weight-bold">
+                            {{ song.name }}
+                        </h1>
+                        <h2>
+                            by {{ song.artist.name }} <br />
+                            {{ song.genre.name }}
+                            {{ secondsToTime(song.duration) }}
+                        </h2>
+                    </div>
+                    <div class="col">
+                        <button
+                            type="button"
+                            class="pl-btn btn btn-secondary m-1 btn-block"
+                        >
+                            <h2 class="m-0">Add to playlist</h2>
+                        </button>
+                        <button
+                            type="button"
+                            class="pl-btn btn btn-primary m-1 btn-block"
+                        >
+                            <h2 class="m-0">Add to session</h2>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </app-layout>
 </template>
 
 <script>
 import AppLayout from "@/Layouts/AppLayout";
 import BackButton from "@/Components/BackButton";
-
+import { helperMixin } from "@/Mixins/HelperMixin";
 export default {
+    mixins: [helperMixin],
     components: {
         AppLayout,
         BackButton,
@@ -26,4 +54,7 @@ export default {
 </script>
 
 <style>
+    .pl-btn {
+        border-radius: 1em;
+    }
 </style>
