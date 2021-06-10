50b3c2ee92bcb1e227267385b1216630bb582e63
Gi0vanniM <g.l.mierop@gmail.com>
2021-06-10 11:11:10 +0200

    add to playlist buttons component

---
+++ resources/js/Components/PlaylistButtonsBig.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="col">
+        <button type="button" class="pl-btn btn btn-secondary m-1 btn-block">
+            <h2 class="m-0">Add to playlist</h2>
+        </button>
+        <button type="button" class="pl-btn btn btn-primary m-1 btn-block">
+            <h2 class="m-0">Add to session</h2>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ resources/js/Components/PlaylistButtonsSmall.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="row">
+        <div class="col px-1">
+            <button
+                type="button"
+                class="pl-btn btn btn-secondary btn-small m-1 btn-block"
+            >
+                Add to playlist
+            </button>
+        </div>
+        <div class="col px-1">
+            <button
+                type="button"
+                class="pl-btn btn btn-primary btn-small m-1 btn-block"
+            >
+                Add to session
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.pl-btn {
+    border-radius: 1em;
+}
+</style>
