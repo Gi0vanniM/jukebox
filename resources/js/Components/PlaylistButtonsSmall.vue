d771622eddb8f7c6f507af763525a39c1fa517ee
Gi0vanniM <g.l.mierop@gmail.com>
2021-06-10 16:30:21 +0200

    added drop down to playlistbuttons that show the user's playlists to add the song to

--- resources/js/Components/PlaylistButtonsSmall.vue
+++ resources/js/Components/PlaylistButtonsSmall.vue
@@ -1,8 +1,24 @@
 <template>
     <div class="btn-group">
-        <button type="button" class="btn btn-secondary btn-small px-1">
+        <button
+            type="button"
+            class="btn btn-secondary btn-small px-1 dropdown-toggle"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+        >
             Add to playlist
         </button>
+        <div class="dropdown-menu">
+            <button
+                v-for="playlist in $page.props.playlists"
+                v-bind:key="playlist.id"
+                type="button"
+                class="btn dropdown-item"
+            >
+                {{ playlist.name }}
+            </button>
+        </div>
 
         <button type="button" class="btn btn-primary btn-small px-1">
             Add to session
