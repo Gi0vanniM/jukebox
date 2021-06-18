661d86f2ba16c0b530193aa5419d52754009cd32
Gi0vanniM <g.l.mierop@gmail.com>
2021-06-18 13:54:40 +0200

    addSong method for saved playlists

--- app/Lib/PlaylistSaved.php
+++ app/Lib/PlaylistSaved.php
@@ -3,12 +3,40 @@
 namespace App\Lib;
 
 use App\Lib\Interfaces\PlaylistInterface;
+use App\Models\Playlist;
+use App\Models\Song;
 
 class PlaylistSaved implements PlaylistInterface
 {
-    public function addSong($id)
+    public $playlist;
+
+    function __construct($playlistId)
     {
-        //
+        $this->playlist = Playlist::find($playlistId);
+    }
+
+    /**
+     * add song method
+     *
+     * @param [type] $id
+     * @param boolean $forced
+     * @return void
+     */
+    public function addSong($id, $forced = false)
+    {
+        // get the song by id
+        $song = Song::find($id);
+
+        if ($this->playlist->songs->contains($song->id) && $forced == false) {
+            return ['songExists' => true];
+        }
+
+        // insert/save the song in the relation table
+        if ($status = $this->playlist->songs()->save($song)) {
+            return ['addedSong' => true];
+        }
+
+        return $status;
     }
 
     public function removeSong($id)
