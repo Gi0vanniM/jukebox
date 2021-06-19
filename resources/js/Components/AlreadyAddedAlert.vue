adb55d8fb7ebcd9570d5fdf6ac0b3fc26750f2d7
Gi0vanniM <g.l.mierop@gmail.com>
2021-06-19 19:53:14 +0200

    song already added alert modal component

---
+++ resources/js/Components/AlreadyAddedAlert.vue
@@ -0,0 +1,83 @@
+<template>
+    <teleport to="#modals">
+        <div
+            class="modal fade"
+            :id="modalId"
+            tabindex="-1"
+            role="dialog"
+            :aria-labelledby="'#' + modalId + '.Label'"
+            aria-hidden="true"
+        >
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <button
+                            type="button"
+                            class="close"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                        >
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+
+                        <h4
+                            class="modal-title font-weight-bold"
+                            :id="'#' + modalId + '.Label'"
+                        >
+                            Already added
+                        </h4>
+
+                        <template v-if="playlistName || playlistId == null">
+                            This is already in your '{{ playlistName }}' playlist.
+                        </template>
+
+                        <div class="d-flex justify-content-center">
+                            <button
+                                type="button"
+                                class="btn"
+                                v-on:click="
+                                    this.$parent.addToPlaylist(
+                                        playlistId,
+                                        songId,
+                                        true
+                                    )
+                                "
+                                data-dismiss="modal"
+                            >
+                                ADD ANYWAY
+                            </button>
+                            <button
+                                type="button"
+                                class="btn btn-primary"
+                                data-dismiss="modal"
+                            >
+                                DON'T ADD
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </teleport>
+</template>
+
+<script>
+export default {
+    props: {
+        songId: {
+            type: Number,
+            required: true,
+        },
+        playlistId: {},
+        playlistName: String,
+    },
+    data() {
+        return {
+            modalId: "addSongAnyway-" + this.songId + "-" + this.playlistId,
+        };
+    },
+};
+</script>
+
+<style>
+</style>
