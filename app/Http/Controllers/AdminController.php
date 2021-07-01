5241dee25c9044da5dc9309c52cc30136480e1a7
Gi0vanniM <g.l.mierop@gmail.com>
2021-07-01 23:05:21 +0200

    admin controller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * display admin dashboard
+     *
+     * @return void
+     */
+    public function index()
+    {
+        //
+    }
+}
