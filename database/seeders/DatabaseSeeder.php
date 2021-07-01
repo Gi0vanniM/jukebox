04d3ca01f4ae9d3ffeb9c78762e6d383cedc9219
Gi0vanniM <g.l.mierop@gmail.com>
2021-07-01 22:09:42 +0200

    call the role seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(GenreSeeder::class);
 
