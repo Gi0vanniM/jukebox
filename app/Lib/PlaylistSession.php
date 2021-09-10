f8459123c02890c6878943ad2c2ec1c16e9ef10e
Gi0vanniM <g.l.mierop@gmail.com>
2021-09-10 11:24:46 +0200

    __construct method for playlist session

--- app/Lib/PlaylistSession.php
+++ app/Lib/PlaylistSession.php
@@ -6,12 +6,27 @@ use App\Lib\Interfaces\PlaylistInterface;
 
 class PlaylistSession implements PlaylistInterface
 {
+    public $playlist;
+
+    function __construct()
+    {
+        if (session('playlist') !== null) {
+            $this->playlist = session('playlist');
+        } else {
+            $this->playlist = [
+                'name' => 'Session playlist',
+                'user' => auth()->user(),
+                'songs' => [],
+            ];
+        }
+    }
+
     public function addSong($id)
     {
         //
     }
 
-    public function removeSong($id)
+    public function removeSong($id, $relationId)
     {
         //
     }
