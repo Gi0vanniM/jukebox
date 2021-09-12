bf348b96d4b6463a0e2493bbb3863b6b2d2977cc
Gi0vanniM <g.l.mierop@gmail.com>
2021-09-12 22:29:18 +0200

    fixed recentSongs() not working and started on adding playlist creation button

--- resources/js/Pages/Playlist/Index.vue
+++ resources/js/Pages/Playlist/Index.vue
@@ -3,9 +3,22 @@
         <template #header>
             <h2 class="h4 font-weight-bold">Your Playlists</h2>
         </template>
-
         <div class="row">
-            <div class="col-12 col-sm-6 col-md-4 pt-2" v-for="playlist in playlists" :key="playlist.id">
+            <div class="col-12 col-sm-6 col-md-4 pt-2 px-1">
+                <button
+                    type="button"
+                    v-on:click="createPlaylistMenu()"
+                    class="card card-link h-100 w-100 bg-dark pb-1"
+                >
+                    <div class="card-body">
+                        <h4 class="card-title"><i class="fas fa-plus"></i></h4>
+                        <p class="card-text">
+                            Create a playlist
+                        </p>
+                    </div>
+                </button>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 pt-2 px-1" v-for="playlist in playlists" :key="playlist.id">
                 <inertia-link
                     :href="route('playlist.show', [playlist.id, playlist.name])"
                     class="card card-link h-100 bg-dark pb-1"
@@ -13,7 +26,7 @@
                     <div class="card-body">
                         <h4 class="card-title">{{ playlist.name }}</h4>
                         <p class="card-text">
-                            {{ recentSongs(playlist) || '...' }}
+                            {{ recentSongs(playlist) || '...'}}
                         </p>
                     </div>
                 </inertia-link>
@@ -33,11 +46,15 @@ export default {
         AppLayout,
     },
     methods: {
-        recentSongs(playlist) {
-            return playlist.songs
+        recentSongs (playlist) {
+            let songs = JSON.parse(JSON.stringify(playlist.songs));
+            return songs
                 .splice(0, 3)
                 .map((x) => x.name)
                 .join(", ");
+        },
+        createPlaylistMenu() {
+            
         },
     },
 };
