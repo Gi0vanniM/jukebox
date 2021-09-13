cfe967e18ee61c5f3e0501e1c04fc1cc04f837ce
Gi0vanniM <g.l.mierop@gmail.com>
2021-09-13 22:21:43 +0200

    saveSession frontend and formatting

--- resources/js/Pages/Playlist/Session.vue
+++ resources/js/Pages/Playlist/Session.vue
@@ -1,9 +1,30 @@
 <template>
     <app-layout>
         <template #header>
-            <small class="font-weight-bold">PLAYLIST</small>
-            <h2 class="h2 font-weight-bold">Your session playlist</h2>
-            <small><b>{{ playlist.user.name }}</b> • {{ playlist.songs.length }} songs, {{totalTime}}</small>
+            <div class="row">
+                <div class="col">
+                    <small class="font-weight-bold">PLAYLIST</small>
+                    <h2 class="h2 font-weight-bold">Your session playlist</h2>
+                    <small
+                        ><b>{{ playlist.user.name }}</b> •
+                        {{ playlist.songs.length }} songs,
+                        {{ totalTime }}</small
+                    >
+                </div>
+                <div class="col">
+                    <div class="float-right">
+                        <div class="btn-group">
+                            <button
+                                v-on:click="saveSession(playlist)"
+                                type="button"
+                                class="btn btn-secondary btn-small px-1"
+                            >
+                                Save session as playlist
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </template>
         <table class="table table-hover border">
             <thead>
@@ -15,7 +36,11 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="(song, index) in playlist.songs" :key="index" :id="'playlist_song-' + song.relationId">
+                <tr
+                    v-for="(song, index) in playlist.songs"
+                    :key="index"
+                    :id="'playlist_song-' + song.relationId"
+                >
                     <td>
                         <inertia-link
                             :href="route('song.show', [song.id, song.name])"
@@ -25,7 +50,14 @@
                     </td>
                     <td>{{ song.artist.name }}</td>
                     <td>{{ secondsToTime(song.duration) }}</td>
-                    <td><playlist-button :currentPlaylist="playlist" :song="song" :relationId="song.relationId" class="float-right"></playlist-button></td>
+                    <td>
+                        <playlist-button
+                            :currentPlaylist="playlist"
+                            :song="song"
+                            :relationId="song.relationId"
+                            class="float-right"
+                        ></playlist-button>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -48,16 +80,61 @@ export default {
     data() {
         return {
             //
-        }
+        };
     },
     computed: {
-        totalTime: function() {
-            return this.secondsToTime(this.playlist.songs.reduce((a, b) => a + b.duration, 0));
-        }
-    }
+        totalTime: function () {
+            return this.secondsToTime(
+                this.playlist.songs.reduce((a, b) => a + b.duration, 0)
+            );
+        },
+    },
+    methods: {
+        saveSession(playlist) {
+            this.$swal
+                .fire({
+                    title: `Save session as playlist?`,
+                    icon: "question",
+                    input: "text",
+                    inputPlaceholder: "enter a name for the playlist",
+                    showCancelButton: true,
+                    confirmButtonColor: "#00FF00",
+                    confirmButtonText: "Save session",
+                    showLoaderOnConfirm: true,
+                    preConfirm: (name) => {
+                        return axios
+                            .post("/api/saveSession", { name: name })
+                            .then((response) => {
+                                console.log(response);
+                                if (response.data.error) {
+                                    this.$swal.showValidationMessage(
+                                        `Request failed: ${response.data.error}`
+                                    );
+                                } else if (response.data.alreadyExists) {
+                                    this.$swal.showValidationMessage(
+                                        `A playlist by the name of "${response.data.name}" already exists!`
+                                    );
+                                }
+                                return response.data;
+                            });
+                    },
+                    allowOutsideClick: () => !this.$swal.isLoading(),
+                })
+                .then((result) => {
+                    if (result.value && result.value.sessionSaved) {
+                        this.$swal.fire({
+                            toast: true,
+                            timer: 2000,
+                            timerProgressBar: true,
+                            title: `Session saved as "${result.value.name}"`,
+                        });
+                        return this.$inertia.visit(`/playlists/${result.value.id}/${result.value.name}`);
+                    }
+                });
+        },
+    },
 };
 </script>
 
 <style>
-
 </style>
